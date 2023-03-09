6e95dbcc2bf53fdb0527f1a3c47293d32b2569d5
FredKy <pinb4llwiz@protonmail.com>
2023-03-09 22:03:50 +0100

    Tested card rendering

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -1,17 +1,86 @@
+import 'dart:math';
+import 'dart:ui';
 
 import 'package:card_game_degree_project/game/player.dart';
+import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/widgets.dart';
 
-class CardGame extends FlameGame {
+import '../models/card.dart';
+
+class CardGame extends FlameGame
+    with HasTappableComponents, HasDraggableComponents {
+  static const double cardWidth = 1000.0;
+  static const double cardHeight = 1400.0;
+  static const double cardGap = 175.0;
+  static const double cardRadius = 100.0;
+  static final Vector2 cardSize = Vector2(cardWidth, cardHeight);
+  static final cardRRect = RRect.fromRectAndRadius(
+    const Rect.fromLTWH(0, 0, cardWidth, cardHeight),
+    const Radius.circular(cardRadius),
+  );
+
   @override
   Future<void> onLoad() async {
-    add(
+    await Flame.images.load('20.png');
+    final sprite = await loadSprite('20.png');
+    print(sprite.srcSize);
+
+    /* add(
       Player()
         ..position = size / 2
         ..width = 50
         ..height = 100
         ..anchor = Anchor.center,
-    );
+    ); */
+
+    final world = World();
+    add(world);
+
+    final random = Random();
+    for (var i = 0; i < 7; i++) {
+      for (var j = 0; j < 4; j++) {
+        final card = Card(id: i * j)
+          ..position = Vector2(100 + i * 1150, 100 + j * 1500)
+          ..addToParent(world);
+        if (random.nextDouble() < 0.9) {
+          // flip face up with 90% probability
+          card.flip();
+        }
+      }
+    }
+
+    world.add(SpriteComponent(
+        sprite: Sprite(
+      Flame.images.fromCache('20.png'),
+      srcPosition: Vector2(300, 300),
+      srcSize: Vector2(512, 512),
+    )));
+
+    final camera = CameraComponent(world: world)
+      ..viewfinder.visibleGameSize =
+          Vector2(cardWidth * 7 + cardGap * 8, 4 * cardHeight + 3 * cardGap)
+      ..viewfinder.position = Vector2(cardWidth * 3.5 + cardGap * 4, 0)
+      ..viewfinder.anchor = Anchor.topCenter;
+    add(camera);
+
+    /* add(
+      SpriteComponent(
+        sprite: sprite,
+        position: size / 2,
+        size: sprite.srcSize / 2,
+        anchor: Anchor.center,
+      ),
+    ); */
   }
+}
+
+Sprite cardGameSprite(double x, double y, double width, double height) {
+  return Sprite(
+    Flame.images.fromCache('20.png'),
+    srcPosition: Vector2(x, y),
+    srcSize: Vector2(width, height),
+  );
 }
--- lib/models/card.dart
+++ lib/models/card.dart
@@ -1,7 +1,19 @@
-class Card {
+import 'dart:math';
+import 'dart:ui';
+
+import 'package:card_game_degree_project/game/game.dart';
+import 'package:flame/components.dart';
+import 'package:flame/experimental.dart';
+
+class Card extends PositionComponent with DragCallbacks {
   String name, description, type;
   int id, cost, power;
   String? imageAssetPath;
+
+  bool _faceUp = false;
+
+  bool get isFaceUp => _faceUp;
+  void flip() => _faceUp = !_faceUp;
 
   Card({
     this.name = "",
@@ -12,4 +24,83 @@ class Card {
     this.power = 0,
     this.imageAssetPath,
   });
+
+  @override
+  void render(Canvas canvas) {
+    if (_faceUp) {
+      _renderFront(canvas);
+    } else {
+      _renderBack(canvas);
+    }
+  }
+
+  void _drawSprite(
+    Canvas canvas,
+    Sprite sprite,
+    double relativeX,
+    double relativeY, {
+    double scale = 1,
+    bool rotate = false,
+  }) {
+    if (rotate) {
+      canvas.save();
+      canvas.translate(size.x / 2, size.y / 2);
+      canvas.rotate(pi);
+      canvas.translate(-size.x / 2, -size.y / 2);
+    }
+    sprite.render(
+      canvas,
+      position: Vector2(relativeX * size.x, relativeY * size.y),
+      anchor: Anchor.center,
+      size: sprite.srcSize.scaled(scale),
+    );
+    if (rotate) {
+      canvas.restore();
+    }
+  }
+
+  static final Paint frontBackgroundPaint = Paint()
+    ..color = const Color.fromARGB(255, 122, 122, 122);
+  static final Paint redBorderPaint = Paint()
+    ..color = const Color(0xffece8a3)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 10;
+  static final Paint blackBorderPaint = Paint()
+    ..color = const Color(0xff7ab2e8)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 10;
+
+  static late final Sprite redJack = cardGameSprite(0, 0, 512, 512);
+
+  void _renderFront(Canvas canvas) {
+    canvas.drawRRect(cardRRect, frontBackgroundPaint);
+    canvas.drawRRect(
+      cardRRect,
+      blackBorderPaint,
+    );
+  }
+
+  static final Paint backBackgroundPaint = Paint()
+    ..color = const Color(0xff380c02);
+  static final Paint backBorderPaint1 = Paint()
+    ..color = const Color(0xffdbaf58)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 10;
+  static final Paint backBorderPaint2 = Paint()
+    ..color = const Color(0x5CEF971B)
+    ..style = PaintingStyle.stroke
+    ..strokeWidth = 35;
+  static final RRect cardRRect = RRect.fromRectAndRadius(
+    CardGame.cardSize.toRect(),
+    const Radius.circular(CardGame.cardRadius),
+  );
+  static final RRect backRRectInner = cardRRect.deflate(40);
+  static late final Sprite flameSprite = cardGameSprite(1367, 6, 512, 512);
+
+  void _renderBack(Canvas canvas) {
+    canvas.drawRRect(cardRRect, backBackgroundPaint);
+    canvas.drawRRect(cardRRect, backBorderPaint1);
+    canvas.drawRRect(backRRectInner, backBorderPaint2);
+    flameSprite.render(canvas, position: size / 2, anchor: Anchor.center);
+  }
 }
